526fd5773755267f5ae67fd1307aca6be4952823
tadaken3 <k.tanaka6057@gmail.com>
2024-04-25 01:50:55 +0900

    ゲームオーバーになったらリセットできるようにした

--- main.py
+++ main.py
@@ -36,10 +36,16 @@ class App:
                 # ジャンプする
                 self.vy = -2.5
                 self.is_jumping = True # ジャンプ中
+            
+            # ゲームオーバーの場合、任意のキーを押すとゲームをリセット
+            elif pressed and not self.is_alive:
+                self.init()
+               
 
     def update(self):
+        #生存していたらっていう条件があるので注意
         if self.is_alive:
-            self.input_key()
+            #self.input_key()
             self.vy += self.gravity
             self.player_y += self.vy
 
@@ -59,18 +65,20 @@ class App:
             if self.player_y > pyxel.height:
                 if self.is_alive:
                     self.is_alive = False
-        else:
-            # ゲームオーバーの場合、任意のキーを押すとゲームをリセット
-            if self.input_key():
-                self.init()
 
-        
     def draw(self):
         pyxel.cls(0)
 
+        ## デバッグ用
+        pyxel.text(10, 10, "player_x: " + str(self.player_x), 2)
+        pyxel.text(10, 20, "player_y: " + str(self.player_y), 2)
+        pyxel.text(10, 40, "is_alive: " + str(self.is_alive), 2)
+        pyxel.text(10, 30, "floor_num: " + str(len(self.floors)), 2)
+        pyxel.text(10, 50, "is_input_key: " + str(self.input_key()), 2)
+
         # 地面を描画
         for floor_x, floor_y, floor_width in self.floors:
-            pyxel.rect(floor_x, floor_y, floor_width, pyxel.height - floor_y, 4)
+            pyxel.rect(floor_x, floor_y, floor_width, 2, 4)
 
 
         #プレイヤーを描画
